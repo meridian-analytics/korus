da59ec942e604a9f722e10e68312fe0456f1bc73
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2025-06-05 06:29:43 -0700

    fix little issues

--- korus/tests/database/interface/test_job.py
+++ korus/tests/database/interface/test_job.py
@@ -30,4 +30,6 @@ def test_get_file_data(job_interface_with_data):
 
     expected = """  channel codec  deployment_id end_utc  file_id filename format  num_samples relative_path  sample_rate start_utc  storage_id
 0     [1]  None              0    None        1  abc.wav   None        40000                       4000      None           0"""
-    assert str(df[sorted(df.columns)]) == expected
+    answer = str(df[sorted(df.columns)])
+
+    assert answer == expected
