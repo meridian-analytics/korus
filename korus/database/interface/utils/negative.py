e90c219c9ecb08ba47f800bbf49b1189b19e9f40
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2025-06-05 14:44:25 -0700

    progress on find periods

--- korus/database/interface/utils/negative.py
+++ korus/database/interface/utils/negative.py
@@ -8,14 +8,13 @@ import numpy as np
 class MonoTimePeriod:
     """TODO: docstring"""
 
-    file_id: int
     deployment_id: int
+    file_id: int
     channel: int
     filename: str
-    file_start_utc: datetime
     start_utc: datetime
+    file_end_utc: datetime
     max_file_gap: float
-    prev_file_end_utc: datetime = None
     end_utc: datetime = None
 
     @property
@@ -24,55 +23,100 @@ class MonoTimePeriod:
 
     def file_gap(self, file_start_utc):
         if self.prev_file_end_utc:
-            return (file_start_utc - self.prev_file_end_utc).total_seconds()
+            return (file_start_utc - self.file_end_utc).total_seconds()
         else:
             return np.inf
 
-    def update(self, filename: str, file_start_utc: datetime) -> bool:
-        # if periods starts after file, update `file_start_utc` and `filename` attributes
-        if self.start_utc >= file_start_utc:
-            self.filename = filename
-            self.file_start_utc = file_start_utc
+    def new_annotation(self, annot_start_utc, annot_end_utc):
+        if annot_start_utc <= self.start_utc:
+            self.start_utc = max(annot_end_utc, self.start_utc)
 
-        # if temporal gap to previous file exceeds the maximum allowed gap, 
-        # end the time period by setting its `end_utc` attribute
-        if self.file_gap(file_start_utc) > self.max_file_gap:
-            self.end_utc = self.prev_file_end_utc
+        else:
+            self.end_utc = annot_start_utc
+
+    def new_file(
+        self,
+        file_id: int,
+        filename: str,
+        file_start_utc: datetime,
+        file_end_utc: datetime,
+    ):
+        # if the period has ended, do nothing
+        if self.has_ended:
+            return
+
+        # if the period starts *after* the new file, update its file attributes
+        if self.start_utc >= file_start_utc:
+            self.file_id = file_id
+            self.filename = filename
+            self.file_end_utc = file_end_utc
+
+        # if the temporal gap to the previous file exceeds the maximum allowed gap, update the period's `end_utc` attribute
+        elif self.file_gap(file_start_utc) > self.max_file_gap:
+            self.end_utc = self.file_end_utc
 
 
 class StereoTimePeriod:
-    def __init__(self, max_file_gap):
+    def __init__(self, deployment_id: int, max_file_gap: float):
+        self.deployment_id = deployment_id
         self.max_file_gap = max_file_gap
         self.mono_periods = dict()
 
-    def update(
+    def new_annotation(
         self,
-        file_id: int,
-        deployment_id: int,
+        channel,
+        annot_start_utc,
+        annot_end_utc,
+    ):
+        # update the current mono time-period
+        self.mono_periods[channel].new_annotation(annot_start_utc, annot_end_utc)
+
+        # start a new mono time-period, if the current one has ended
+        p = self.mono_periods[channel]
+        if p.has_ended:
+            self.mono_periods[channel] = MonoTimePeriod(
+                deployment_id=self.deployment_id,
+                max_file_gap=self.max_file_gap,
+                file_id=p.file_id,
+                channel=channel,
+                filename=p.filename,
+                file_end_utc=p.file_end_utc,
+                start_utc=annot_end_utc,
+            )
+
+        # return the updated mono time-period (*not* the new one, if a new one was created)
+        return p
+
+    def new_file(
+        self,
         channel: int,
+        file_id: int,
         filename: str,
         file_start_utc: datetime,
+        file_end_utc: datetime,
     ) -> MonoTimePeriod:
         # update the current mono time-period
-        p = self.mono_periods.get(channel, None)
-        if p and not p.has_ended:
-            self.mono_periods[channel].update(filename, file_start_utc)
+        if channel in self.mono_periods:
+            self.mono_periods[channel].new_file(
+                file_id, filename, file_start_utc, file_end_utc
+            )
 
         # start a new mono time-period, if there is none or the current one has ended
         p = self.mono_periods.get(channel, None)
         if not p or p.has_ended:
             self.mono_periods[channel] = MonoTimePeriod(
+                deployment_id=self.deployment_id,
+                max_file_gap=self.max_file_gap,
                 file_id=file_id,
-                deployment_id=deployment_id,
                 channel=channel,
                 filename=filename,
-                file_start_utc=file_start_utc,
+                file_end_utc=file_end_utc,
                 start_utc=file_start_utc,
-                max_file_gap=self.max_file_gap,
             )
 
-        # return the current mono time-period (*not* the new one, if a new one was created)
+        # return the updated mono time-period (*not* the new one, if a new one was created)
         return p
+
 
 def find_empty_periods(
     files: pd.DataFrame, annots: pd.DataFrame, max_file_gap: float = 0.1
@@ -114,32 +158,56 @@ def find_empty_periods(
     annots.sort_values(by="start_utc", inplace=True)
 
     # reindex
-    files = files.reset_index().set_index(["deployment_id", "start_utc"])
+    files = files.reset_index().set_index(["deployment_id", "start_utc", "end_utc"])
     annots = annots.reset_index().annots.set_index(
-        ["deployment_id", "channel", "start_utc"]
+        ["deployment_id", "channel", "start_utc", "end_utc"]
     )
 
     periods = []
 
     # loop over deployments
-    for deploy_id, files_deploy in files.groupby(level=0):
-        annots_deploy = annots.loc[deploy_id]
+    for deployment_id, files_deploy in files.groupby(level=0):
+        annots_deploy = annots.loc[deployment_id]
 
-        stereo_period = StereoTimePeriod(max_file_gap)
+        stereo_period = StereoTimePeriod(deployment_id, max_file_gap)
 
         # loop over files
-        for (_, file_start_utc), file_row in files_deploy.iterrows():
+        for (_, file_start_utc, file_end_utc), file_row in files_deploy.iterrows():
 
             # loop over channels
             for channel in file_row.channel:
 
-                mono_period = stereo_period.update(
-                    file_id = file_row.file_id,
-                    deployment_id = deploy_id,
-                    channel = channel,
-                    filename = file_row.filename,
-                    file_start_utc = file_start_utc,                    
+                # update the current stereo period with the new file
+                mono_period = stereo_period.new_file(
+                    channel=channel,
+                    file_id=file_row.file_id,
+                    filename=file_row.filename,
+                    file_start_utc=file_start_utc,
+                    file_end_utc=file_end_utc,
                 )
 
+                # if the period has ended, save it to the list
                 if mono_period.has_ended:
                     periods.append(mono_period)
+
+                try:
+                    # select annotations for current channel
+                    annots_chan = annots_deploy.loc[channel]
+
+                except KeyError:
+                    # if there are none, proceed to the next channel
+                    continue
+
+                # select annotations that have start time within current file
+                annots_file = annots_chan.loc[
+                    (annots_chan.index >= file_start_utc)
+                    & (annots_chan.index < file_end_utc)
+                ]
+
+                # loop over annotations
+                for (start_utc, end_utc), _ in annots_file.iterrows():
+                    mono_period = stereo_period.new_annotation(start_utc, end_utc)
+
+                    # if the period has ended, save it to the list
+                    if mono_period.has_ended:
+                        periods.append(mono_period)
