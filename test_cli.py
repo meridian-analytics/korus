939c06629857777d383ec1c6e22cefedce472a53
oliskir <oliver.kirsebom@gmail.com>
2025-07-26 08:03:41 -0700

    cli

--- test_cli.py
+++ test_cli.py
@@ -1,14 +1,5 @@
-import os
-import pytest
 import korus.cli.cli as cli
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-path_to_assets = os.path.join(current_dir, "assets")
-path_to_tmp = os.path.join(path_to_assets, "tmp")
-
-
-def test_test():
-    pass
 
 
 cli.cli_fcn("abc")
