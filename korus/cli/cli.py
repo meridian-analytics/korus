00d3cf83ac99356b8c30a333bf16f1cf8baa852f
oliskir <oliver.kirsebom@gmail.com>
2025-07-26 07:52:00 -0700

    cli

--- korus/cli/cli.py
+++ korus/cli/cli.py
@@ -9,20 +9,25 @@ def table_options(answers):
     else:
         return ["c","d"]
 
-questions = [
-    inquirer.List(
-        name="table", 
-        message="Select a table",
-        choices=["deployment", "annotation", "Exit"]    
-    ),
-    inquirer.List(
-        name="options",
-        message="Select an option",
-        choices=table_options        
-    )
-]
+while True:
 
-answers = inquirer.prompt(questions)
+    questions = [
+        inquirer.List(
+            name="table", 
+            message="Select a table",
+            choices=["deployment", "annotation", "Exit"]    
+        ),
+        inquirer.List(
+            name="options",
+            message="Select an option",
+            choices=table_options        
+        )
+    ]
 
+    answers = inquirer.prompt(questions)
 
-print(answers)
+    if answers is None:
+        print("Terminating ...")
+        break
+
+    print(answers)
