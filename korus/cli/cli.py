b316c3e63cb47962c84d3eba8e022e0349435537
oliskir <oliver.kirsebom@gmail.com>
2025-07-26 07:50:53 -0700

    cli

--- korus/cli/cli.py
+++ korus/cli/cli.py
@@ -2,11 +2,23 @@ import inquirer
 
 #https://python-inquirer.readthedocs.io/en/latest/usage.html#question-types
 
+
+def table_options(answers):
+    if answers["table"] == "deployment":
+        return ["a","b"]
+    else:
+        return ["c","d"]
+
 questions = [
     inquirer.List(
         name="table", 
         message="Select a table",
         choices=["deployment", "annotation", "Exit"]    
+    ),
+    inquirer.List(
+        name="options",
+        message="Select an option",
+        choices=table_options        
     )
 ]
 
