dc892353ef5ae894672e0f70c51197721aba09a6
oliskir <oliver.kirsebom@gmail.com>
2025-09-22 06:06:50 -0700

    progress

--- korus/cli/parse.py
+++ korus/cli/parse.py
@@ -48,6 +48,22 @@ def create_validate_datetime(required=False):
     return validate
 
 
+def _parse_any(fcn, answers, current, required=False, return_bool=False):
+    if required and current == "":
+        raise inquirer.errors.ValidationError(
+            "", reason="A non-null value is required for this field."
+        )
+
+    try:
+        v = fcn(current)
+        return True if return_bool else v
+
+    except:
+        raise inquirer.errors.ValidationError(
+            "", reason="Invalid input. Please enter a valid floating-point number."
+        )
+
+
 def parse_value(field, value):
     if field.type == datetime:
         return parse_datetime(None, current=value, required=field.required)
@@ -58,40 +74,23 @@ def parse_value(field, value):
     elif field.type == float:
         return parse_float(None, current=value, required=field.required)
 
+    elif field.type == bool:
+        return parse_bool(None, current=value, required=field.required)
+
     else:
         return value
 
 
+def parse_bool(answers, current, required=False, return_bool=False):
+    return _parse_any(lambda x: bool(x), answers, current, required, return_bool)
+
+
 def parse_float(answers, current, required=False, return_bool=False):
-    if required and current == "":
-        raise inquirer.errors.ValidationError(
-            "", reason="A non-null value is required for this field."
-        )
-
-    try:
-        v = float(current) if current != "" else None
-        return True if return_bool else v
-
-    except:
-        raise inquirer.errors.ValidationError(
-            "", reason="Invalid input. Please enter a valid floating-point number."
-        )
+    return _parse_any(lambda x: float(x), answers, current, required, return_bool)
 
 
 def parse_int(answers, current, required=False, return_bool=False):
-    if required and current == "":
-        raise inquirer.errors.ValidationError(
-            "", reason="A non-null value is required for this field."
-        )
-
-    try:
-        v = int(current) if current != "" else None
-        return True if return_bool else v
-
-    except:
-        raise inquirer.errors.ValidationError(
-            "", reason="Invalid input. Please enter a valid integer number."
-        )
+    return _parse_any(lambda x: int(x), answers, current, required, return_bool)
 
 
 DATETIME_FORMAT = "YYYY-MM-DD HH:MM:SS.SSS"
